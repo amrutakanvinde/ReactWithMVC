63f3515caa512816456d3075bcf5e3a43094f083
amrutakanvinde <kanvinde.amruta@gmail.com>
2019-02-26 21:06:18 -0500

    created new notes list

---
+++ ReactwithMVC/Controllers/NotesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReactwithMVC.Controllers
+{
+    public class NotesController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View ();
+        }
+    }
+}
