f6064f2faf61a213c6ecbef7ac6e860ec2428f9d
amrutakanvinde <kanvinde.amruta@gmail.com>
2019-03-04 12:10:29 -0500

    Server Side entries displayed

--- ReactwithMVC/Controllers/NotesController.cs
+++ ReactwithMVC/Controllers/NotesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ReactwithMVC.Models;
 
 namespace ReactwithMVC.Controllers
 {
@@ -10,7 +11,27 @@ namespace ReactwithMVC.Controllers
     {
         public ActionResult Index()
         {
+
             return View ();
+        }
+
+        public List<Note> NotesDeclare()
+        {
+            return new List<Note>()
+            {
+                new Note {Id = 1, Name = "Server Side Note 1", DateTime_Added = DateTime.Today },
+                new Note {Id = 2, Name = "Server Side Note 2", DateTime_Added = DateTime.Today },
+                new Note {Id = 3, Name = "Server Side Note 3", DateTime_Added = DateTime.Today },
+            };
+        }
+
+        public JsonResult GetNotes()
+        {
+            return new JsonResult
+            {
+                Data = NotesDeclare(),
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet
+            };
         }
     }
 }
---
+++ ReactwithMVC/Models/Note.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace ReactwithMVC.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }   
+
+        public string Name { get; set; }
+
+        public DateTime DateTime_Added { get; set; }
+
+    }
+}
